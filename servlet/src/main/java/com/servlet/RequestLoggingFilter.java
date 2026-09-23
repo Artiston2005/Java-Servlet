c66c6eed1fdf332f3579588737ef6049f927e005
Artiston2005 <ashwinyadav2408@gmail.com>
2026-09-23 08:20:04 +0530

    Initial commit

---
+++ servlet/src/main/java/com/servlet/RequestLoggingFilter.java
@@ -0,0 +1,30 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
+@WebFilter("/*")
+public class RequestLoggingFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req =
+                (HttpServletRequest) request;
+
+        System.out.println("Request URI: " +
+                           req.getRequestURI());
+
+        chain.doFilter(request, response);
+    }
+}
